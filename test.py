f9d740853b5994257e9ffbe1e16455a1f44fac89
Maksimov-Dmitry <61335724+Maksimov-Dmitry@users.noreply.github.com>
2021-09-23 17:55:48 +0300

    fix

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
 from main import download
+import pytest
+
+@pytest.fixture(scope="session")
+def browser_type_launch_args(browser_type_launch_args):
+    return {
+        **browser_type_launch_args,
+        "headless": False
+    }
+
+@pytest.fixture()
+def browser_context_args(browser_context_args):
+    return {
+        **browser_context_args,
+        "accept_downloads": True
+    }
+
 
 def test_download(page):
-    assert download(page).failure() is not None
+    assert download(page).failure() is None
